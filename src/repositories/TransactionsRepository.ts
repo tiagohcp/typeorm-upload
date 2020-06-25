f1f97b0b664c62fd49e5e60c71153665fb6bd087
Tiago Pereira <tiagohcp@gmail.com>
2020-06-24 22:53:23 -0300

    Add Transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,44 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
+  private transactions: Transaction[];
+
+  public async find(): Promise<Transaction[]> {
+    this.transactions = await super.find({
+      relations: ['category'],
+    });
+
+    return this.transactions;
+  }
+
   public async getBalance(): Promise<Balance> {
-    // TODO
+    if (!this.transactions) {
+      this.transactions = await this.find();
+    }
+
+    const sumIncome = this.transactions
+      .filter(types => types.type === 'income')
+      .map(values => values.value)
+      .reduce(
+        (accumulatedValue, current): number => accumulatedValue + current,
+        0,
+      );
+
+    const sumOutcome = this.transactions
+      .filter(types => types.type === 'outcome')
+      .map(values => values.value)
+      .reduce(
+        (accumulatedValue, current): number => accumulatedValue + current,
+        0,
+      );
+
+    const balance: Balance = {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    };
+
+    return balance;
   }
 }
 
